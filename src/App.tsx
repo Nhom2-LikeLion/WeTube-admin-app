d489c03e8c0607a8d9bcdabb9ecf13cf2603bd94
Kira366 <khaitran.k366@gmail.com>
2025-07-10 16:00:56 +0700

    update structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className="text-3xl font-bold text-red-500">Hello Tailwind + Vite</h1>
+      <div className='w-screen h-screen flex bg-amber-300 border'  >
+        HelloWorld
+      </div>
     </>
   )
 }
